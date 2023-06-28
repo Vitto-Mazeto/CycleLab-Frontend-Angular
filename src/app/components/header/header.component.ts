ed956c4dbc890713eb6f9d66cb16134348d5cfbc
vitto.mazeto <vittopicma@gmail.com>
2023-06-28 16:09:36 -0300

    feat: dinamic header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isLoggedIn = false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Verifica se a rota atual é a de login
+        this.isLoggedIn = (event.url !== '/login');
+      }
+    });
+  }
 }
