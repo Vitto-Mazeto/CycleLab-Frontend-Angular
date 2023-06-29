1f11a784bf156280cf8201f08749a53fa8680c2c
vitto.mazeto <vittopicma@gmail.com>
2023-06-29 19:25:39 -0300

    feat: invalid login error message

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,6 +12,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
   userLogin = new UserLoginViewModel();
   formulario!: FormGroup;
+  invalidAccount: boolean = false;
 
   constructor(private formBuilder: FormBuilder,
     private router: Router,
@@ -31,19 +32,29 @@ export class LoginComponent implements OnInit {
     if (this.formulario.valid) {
       this.userLogin.email = this.formulario.get('email')?.value;
       this.userLogin.senha = this.formulario.get('senha')?.value;
-      this.authService.login(this.userLogin).subscribe((response) => {
-        if (response.sucesso && response.token) {
-          const token = response.token;
-
-          localStorage.setItem('token', token);
-
-          this.router.navigate(['/homepage']);
-      }
-    });
+      this.authService.login(this.userLogin).subscribe({
+        next: (response) => {
+          if (response.sucesso && response.token) {
+            const token = response.token;
+            localStorage.setItem('token', token);
+            this.router.navigate(['/homepage']);
+          } else {
+            console.log('Usuário ou senha inválidos');
+            this.formulario.get('senha')?.setErrors({ invalidAccount: true });
+          }
+        },
+        error: (error) => {
+          if (error.status === 401) {
+            this.formulario.get('senha')?.setErrors({ invalidAccount: true });
+          } else {
+            console.error('Ocorreu um erro durante a solicitação de login:', error);
+          }
+        }
+      });
     } else {
       console.log('Formulário inválido');
       this.formulario.get('email')?.markAsTouched();
-      this.formulario.get('password')?.markAsTouched();
+      this.formulario.get('senha')?.markAsTouched();
     }
   }
 
