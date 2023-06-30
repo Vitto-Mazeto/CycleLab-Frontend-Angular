55cd5a91853eff502c827801cbe3f9a69d781c01
vitto.mazeto <vittopicma@gmail.com>
2023-06-30 17:10:40 -0300

    refactor: register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserRegisterViewModel } from 'src/app/models/userRegisterViewModel';
 import { UserLoginViewModel } from 'src/app/models/userLoginViewModel';
@@ -14,63 +14,80 @@ export class RegisterComponent implements OnInit {
   userRegister = new UserRegisterViewModel();
   formulario!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private router: Router,
-    private authService: AuthService) { }
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.formulario = this.formBuilder.group({
-      email: ['', Validators.compose([
-        Validators.required,
-        Validators.email
-      ])],
-      senha: ['', Validators.compose([
-        Validators.required,
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{6,}$')
-      ])],
-      isAdmin: new FormControl(false), // Adiciona a propriedade isAdmin com o valor inicial como falso
-      senhaConfirmacao: ['', Validators.required]
-    }, { validator: this.confirmaSenhaValidator });
+    this.initializeForm();
   }
 
-  confirmaSenhaValidator(group: FormGroup): any {
+  initializeForm(): void {
+    this.formulario = this.formBuilder.group(
+      {
+        email: ['', Validators.compose([Validators.required, Validators.email])],
+        senha: ['', Validators.compose([Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{6,}$')])],
+        isAdmin: [false],
+        senhaConfirmacao: ['', Validators.required]
+      },
+      { validators: this.confirmaSenhaValidator }
+    );
+  }
+  
+  confirmaSenhaValidator(group: AbstractControl): ValidationErrors | null {
     const senha = group.get('senha')?.value;
     const confirmaSenha = group.get('senhaConfirmacao')?.value;
-
+  
     if (senha === confirmaSenha) {
       group.get('senhaConfirmacao')?.setErrors(null);
+      return null; // retorna null quando a validação é satisfeita
     } else {
       group.get('senhaConfirmacao')?.setErrors({ senhaMismatch: true });
+      return { senhaMismatch: true };
     }
-
-    return null;
   }
 
   submitForm(): void {
-    if (this.formulario.valid) {
-      this.userRegister.email = this.formulario.get('email')?.value;
-      this.userRegister.senha = this.formulario.get('senha')?.value;
-      this.userRegister.senhaConfirmacao = this.formulario.get('senhaConfirmacao')?.value;
-      this.userRegister.isAdmin = this.formulario.get('isAdmin')?.value;
-  
-      this.authService.register(this.userRegister).subscribe((response) => { 
-        if (response.sucesso) {
-          let userLogin = new UserLoginViewModel();
-          userLogin.email = this.userRegister.email;
-          userLogin.senha = this.userRegister.senha;
-          this.authService.login(userLogin).subscribe((response) => {
-            if (response.sucesso && response.token) {
-              const token = response.token;
-  
-              localStorage.setItem('token', token);
-  
-              this.router.navigate(['/homepage']);
-            }
-          });
-        }
+    if (this.isFormValid()) {
+      this.setUserRegisterData();
+      this.authService.register(this.userRegister).subscribe((response) => {
+        this.handleRegisterResponse(response);
       });
     } else {
       this.formulario.markAllAsTouched();
+    }
+  }
+
+  isFormValid(): boolean {
+    return this.formulario.valid;
+  }
+
+  setUserRegisterData(): void {
+    this.userRegister.email = this.formulario.get('email')?.value;
+    this.userRegister.senha = this.formulario.get('senha')?.value;
+    this.userRegister.senhaConfirmacao = this.formulario.get('senhaConfirmacao')?.value;
+    this.userRegister.isAdmin = this.formulario.get('isAdmin')?.value;
+  }
+
+  handleRegisterResponse(response: any): void {
+    if (response.sucesso) {
+      const userLogin = new UserLoginViewModel();
+      userLogin.email = this.userRegister.email;
+      userLogin.senha = this.userRegister.senha;
+
+      this.authService.login(userLogin).subscribe((response) => {
+        this.handleLoginResponse(response);
+      });
+    }
+  }
+
+  handleLoginResponse(response: any): void {
+    if (response.sucesso && response.token) {
+      const token = response.token;
+      localStorage.setItem('token', token);
+      this.router.navigate(['/homepage']);
     }
   }
 }
