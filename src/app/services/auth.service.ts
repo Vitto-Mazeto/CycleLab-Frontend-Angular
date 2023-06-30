499396afa7ca14f7b562ff6fff6fe34d6e02e72b
vitto.mazeto <vittopicma@gmail.com>
2023-06-30 18:33:26 -0300

    refactor: authservice

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,26 +1,26 @@
-import { UserLoginViewModel } from './../models/userLoginViewModel';
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { UserRegisterViewModel } from '../models/userRegisterViewModel';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+import { UserLoginViewModel } from './../models/userLoginViewModel';
+import { UserRegisterViewModel } from '../models/userRegisterViewModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private baseUrl = 'https://localhost:7237/User';
+
   constructor(private http: HttpClient) { }
-  // Ajustar o any
+
   public register(user: UserRegisterViewModel): Observable<any> {
-    return this.http.post<any>(
-      'https://localhost:7237/User/cadastro',
-      user);
+    const url = `${this.baseUrl}/cadastro`;
+    return this.http.post<any>(url, user);
   }
 
-
   public login(user: UserLoginViewModel): Observable<any> {
-    return this.http.post<any>(
-      'https://localhost:7237/User/login',
-      user);
+    const url = `${this.baseUrl}/login`;
+    return this.http.post<any>(url, user);
   }
 }
